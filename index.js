44d5b8426ffc2ae651324683070f18a83dfe082e
Tasreen Mowreen <tmowreen97@gmail.com>
2022-03-11 00:37:57 -0500

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-function addingEventListener() {
-    const input = document.getElementById('input');
+const input = document.getElementById('input')
 
-    function clickAlert() {
-      alert('I was clicked!');
-    };
-    
-    input.addEventListener('click', clickAlert);
+
+function addingEventListener(){
+  return (input.addEventListener('click', () => alert('I was clicked!')))
 }
-
